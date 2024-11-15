7fda82c3b7e409222bb6e56213c736e3856384bc
RCKNDRLLA <sokolov.yakov@inbox.ru>
2024-11-15 15:18:04 +0500

    3.4.1

--- onetoone/models.py
+++ onetoone/models.py
@@ -9,3 +9,4 @@ class Account(models.Model):
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
